5e1e7c6026dd268367683cbccd38eb45c16180f8
Simon Thörnqvist <simon@plug-n-trade.com>
2017-01-29 12:16:13 +0100

    init project

---
+++ lib/ex_wss.ex
@@ -0,0 +1,18 @@
+defmodule ExWss do
+  @moduledoc """
+  Documentation for ExWss.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWss.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_wss_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWssTest do
+  use ExUnit.Case
+  doctest ExWss
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
