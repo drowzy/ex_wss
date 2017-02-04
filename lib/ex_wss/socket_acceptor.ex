5c33c0ede812c676ebc3bd3eb0b359d983630e66
Simon Thörnqvist <simon@plug-n-trade.com>
2017-02-04 23:37:09 +0100

    comple pub/sub example

--- lib/ex_wss/socket_acceptor.ex
+++ lib/ex_wss/socket_acceptor.ex
@@ -12,7 +12,9 @@ defmodule ExWss.SocketAcceptor do
     registry_key = Keyword.get(opts, :registry_key)
     state = %{registry_key: registry_key}
 
-    {:ok, req, state, timeout}
+    IO.puts("Connected client #{Kernel.inspect(self())}")
+
+    {:ok, req, state}
   end
 
   # Handle 'ping' messages from the browser - reply
@@ -20,38 +22,57 @@ defmodule ExWss.SocketAcceptor do
     {:reply, {:text, "pong"}, req, state}
   end
 
-  def websocket_handle({:text, "register"}, req, state) do
-    {:reply, {:text, try_register()}, req, state}
+  # Handle other messages from client, either we
+  # subscribe, or publish
+  def websocket_handle({:text, content}, req, state) do
+    resp = content
+    |> Poison.decode!
+    |> action(state)
+    |> Poison.encode!
+
+    {:reply, {:text, resp}, req, state}
   end
 
-  # Handle other messages from the browser - don't reply
-  def websocket_handle({:text, message}, req, state) do
-    IO.puts(message)
-    {:ok, req, state}
-  end
-
-  # Format and forward elixir messages to client
-  def websocket_info(message, req, state) when is_binary(message) do
-    IO.puts(message)
-    {:reply, {:text, message}, req, state}
-  end
-
-  def websocket_info({:deliver, message}, req, state) do
+  # Messages received through the info callback are from
+  # other elixir processes, for instance when a publish
+  # is performed on a topic this process subscribes on
+  # we send the messge to the client
+  def websocket_info({:broadcast, message}, req, state) do
     {:reply, {:text, message}, req, state}
   end
 
   # No matter why we terminate, remove all of this pids subscriptions
   def websocket_terminate(reason, _req, _state) do
-    IO.puts("terminated")
-    IO.puts(reason)
     :ok
   end
 
-  defp try_register() do
-    case Registry.register(@registry_name, "sub", []) do
-      {:ok, _} -> "registred"
-      _ -> "Already registred"
+  defp action(msg = %{"type" => "subscribe"}, %{registry_key: registry_key}) do
+    case Registry.register(registry_key, Map.get(msg, :topic), []) do
+      {:ok, _} -> ack()
+      _ -> nack()
     end
+  end
+
+  defp action(msg = %{"type" => "publish", "payload" => payload}, %{registry_key: registry_key}) do
+    pid = self()
+
+    Registry.dispatch(registry_key, Map.get(msg, :topic), fn entries ->
+      for {p, _} <- entries, p != pid, do: send(p, {:broadcast, payload})
+    end)
+
+    ack()
+  end
+
+  defp action(_) do
+    nack()
+  end
+
+  defp ack do
+    %{type: "ack"}
+  end
+
+  defp nack do
+    %{type: "nack"}
   end
 
 end
